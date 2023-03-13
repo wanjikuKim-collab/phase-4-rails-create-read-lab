2eff570cd67359de9757deb0d4f49453694cff2c
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-03-14 00:40:50 +0300

    done

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,30 @@
+class PlantsController < ApplicationController
+    wrap_parameters format: []
+
+    # GET /plants
+    def index
+        render json: Plant.all, status: :ok
+    end
+
+    # GET /plants/:id
+    def show
+        plant = Plant.find_by(id: params[:id])
+        if plant
+            render json: plant, status: :ok
+        else
+            render json: {error: "Plant not found"}, status: :not_found
+       end
+    end
+
+    # POST /plants
+    def create
+        plant = Plant.create(plant_params)
+        render json: plant, status: :created
+    end
+
+    private
+
+    def plant_params
+        params.permit(:name, :image, :price)
+    end
+end
